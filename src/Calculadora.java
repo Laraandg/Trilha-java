60e83499edc01701f525d44e3db596c9505f29be
Lara Andrade <lara.andradeg@gmail.com>
2023-10-14 20:48:56 -0300

    novas classes add

---
+++ src/Calculadora.java
@@ -0,0 +1,25 @@
+    /**
+     * <h1>Calculadora</h1>
+     * A Calculadora realiza opera&ccedil;&otilde;es matem&aacute;ticas entre n&uacute;meros inteiros
+     * <p>
+     * <b>Note:</b> Leia atentamente a documenta&ccedil;&atilde;o desta classes
+     * para desfrutar dos recursos oferecidos pelo autor
+     *
+     * @author Gleyson Sampaio
+     * @version 1.0
+     * @since 01/01/2022
+     */
+    public class Calculadora {
+
+        /**
+         * Este método é utilizado para somar dois números inteiros
+         * @param numeroUm este é o primeiro parâmetro do método
+         * @param numeroDois este é o segundo parâmetro do método
+         * @return int o resultado deste método é a soma dos dois números.
+         */
+        public int somar(int numeroUm, int numeroDois) {
+            return  numeroUm + numeroDois;
+        }
+    }
+
+
---
+++ src/Comentarios.java
@@ -0,0 +1,24 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //olá, eu sou um comentário em uma linha única
+
+        /* Olá,
+         * Eu sou um comentario
+         * que posso ser mais detalhadod
+         * quando necessário
+         */
+    }
+
+            /**
+             * Estas duas estrelinhas acima
+             * é para identificar que você
+             * pretende elaborar um comentário
+             * a nível de documentação.
+             * Que incrível !!!
+             */
+
+public void metodo(){
+
+        }
+    }
+
